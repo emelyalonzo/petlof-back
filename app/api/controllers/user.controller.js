42762a5758a76befde5dd21713250cb463f3730c
Emely Alonzo <emelyalonzom@gmail.com>
2022-05-30 22:59:10 +0200

    undefinded

--- app/api/controllers/user.controller.js
+++ app/api/controllers/user.controller.js
@@ -229,10 +229,6 @@ const addMatch = async (req, res, next) => {
 
     console.log("matches of user who made the match BEFORE", user.matches);
 
-    // let matchesUser = user.matches;
-    // matchesUser.push(matchedUser);
-    // console.log(matchedUser);
-    // user.matches = matchesUser;
     console.log("matches of user" , user.matches)
 
     console.log("matches of user who made the match AFTER", user.matches);
